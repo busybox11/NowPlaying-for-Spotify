1c61454bdb0ccb25a354de8087947963f888ff78
busybox <planetgeek11@gmail.com>
2019-09-02 16:13:47 +0200

    Updated translations for Open Source text

--- lang/fr.php
+++ lang/fr.php
@@ -3,6 +3,8 @@ define('IndexWelcome', 'Bienvenue sur NowPlaying for Spotify !');
 define('IndexPleaseConnect', 'Veuillez vous connecter avec votre compte Spotify.');
 define('IndexConnection', 'CONNEXION');
 define('IndexCookie', 'En cliquant sur \'CONNEXION\', vous acceptez l\'utilisation des cookies nécessaires au bon fonctionnement du site.');
+define('OpenSourceProject', 'Ce projet est toralement Open Source et est disponible sur GitHub ');
+define('OpenSourceLink', 'à cette adresse.');
 
 define('defaultTitleSong', 'Aucune musique en cours de lecture');
 define('defaultArtistSong', 'Veuillez patienter quelques secondes pour l\'actualisation');
