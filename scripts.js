583cb1277d35cb5204d49d582b854d132e130ae9
busybox <planetgeek11@gmail.com>
2019-11-17 16:44:06 +0100

    Added sleep.js

--- scripts.js
+++ scripts.js
@@ -18,3 +18,46 @@ function readCookie(name) {
     }
     return null;
 }
+
+// sleep.js
+// https://github.com/ivanmaeder/computer-sleep
+
+var sleep = {
+    prevent: function() {
+        if (!this._video) {
+            this._init();
+        }
+
+        this._video.setAttribute('loop', 'loop');
+        this._video.play();
+    },
+    allow: function() {
+        if (!this._video) {
+            return;
+        }
+
+        this._video.removeAttribute('loop');
+        this._video.pause();
+    },
+    _init: function() {
+        this._video = document.createElement('video');
+        this._video.setAttribute('width', '10');
+        this._video.setAttribute('height', '10');
+        this._video.style.position = 'absolute';
+        this._video.style.top = '-10px';
+        this._video.style.left = '-10px';
+
+        var source_mp4 = document.createElement('source');
+        source_mp4.setAttribute('src', 'https://github.com/ivanmaeder/computer-sleep/raw/master/resources/muted-blank.mp4');
+        source_mp4.setAttribute('type', 'video/mp4');
+        this._video.appendChild(source_mp4);
+
+        var source_ogg = document.createElement('source');
+        source_ogg.setAttribute('src', 'https://github.com/ivanmaeder/computer-sleep/raw/master/resources/muted-blank.ogv');
+        source_ogg.setAttribute('type', 'video/ogg');
+        this._video.appendChild(source_ogg);
+
+        document.body.appendChild(this._video);
+    },
+    _video: null
+}
