ec1ebc27c069cfe891e19a73eaf654548b10cda2
busybox <planetgeek11@gmail.com>
2020-02-09 15:13:16 +0100

    v1.5 - Added playback
    Web Playback SDK used to stream music directly on webpage

--- playing.php
+++ playing.php
@@ -157,4 +157,31 @@ switch($_COOKIE['lang']){
         </div>
     </div>
     <div id="background-image-div" style="background: url('no_song.png'); background-size: cover;background-position: center center;"><div class="darken"></div></div>
+    <script src="https://sdk.scdn.co/spotify-player.js"></script>
+    <script>
+    window.onSpotifyWebPlaybackSDKReady = () => {
+    const token = readCookie('accessToken');
+    const player = new Spotify.Player({
+    name: 'NowPlaying for Spotify',
+    getOAuthToken: cb => { cb(token); }
+    });
+    // Error handling
+    player.addListener('initialization_error', ({ message }) => { console.error(message); });
+    player.addListener('authentication_error', ({ message }) => { console.error(message); });
+    player.addListener('account_error', ({ message }) => { console.error(message); });
+    player.addListener('playback_error', ({ message }) => { console.error(message); });
+    // Playback status updates
+    player.addListener('player_state_changed', state => { console.log(state); });
+    // Ready
+    player.addListener('ready', ({ device_id }) => {
+    console.log('Ready with Device ID', device_id);
+    });
+    // Not Ready
+    player.addListener('not_ready', ({ device_id }) => {
+    console.log('Device ID has gone offline', device_id);
+    });
+    // Connect to the player!
+    player.connect();
+    };
+    </script>
 </body>
