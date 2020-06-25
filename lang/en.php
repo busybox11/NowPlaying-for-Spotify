3631baea6818c2e6078d99d69dcefc773ed5baf0
busybox <planetgeek11@gmail.com>
2020-06-26 00:18:29 +0200

    Updated variables and sentences

--- lang/en.php
+++ lang/en.php
@@ -1,21 +1,17 @@
 <?php
-define('IndexWelcome', 'Welcome on NowPlaying for Spotify !');
-define('IndexPleaseConnect', 'Please login with your Spotify account.');
-define('IndexConnection', 'LOGIN');
-define('IndexCookie', 'By clicking on \'LOGIN\', you accept the use of cookies necessary for the proper functioning of the site.');
-define('OpenSourceProject', 'This project is totally Open Source and is available on GitHub ');
-define('OpenSourceLink', 'here.');
+define('IndexTitle', 'Welcome to NowPlaying for Spotify !');
+define('IndexDescription', 'Please login with your Spotify account.');
+define('IndexConnection', 'Login to Spotify');
+define('IndexCookie', 'By clicking on \'LOGIN TO SPOTIFY\', you accept the use of cookies necessary for the proper functioning of the site.');
 
-define('DiscordServer', 'Join our Discord server !');
-
-define('defaultTitleSong', 'No music being played');
-define('defaultArtistSong', 'Please wait a few seconds for the update');
+define('defaultTitleSong', 'No music currently playing');
+define('defaultArtistSong', 'Please wait a few seconds while status is updating');
 define('defaultTitle', 'No music - Spotify Connect - Now Playing');
 define('ad', 'Advertisement');
 
 define('by', 'by');
 
 define('Error', 'Error');
-define('IndexError404', 'This page doesn\'t exist. Aren\'t you trying to hack me ?');
+define('IndexError404', 'This page doesn\'t exist. Ain\'t you trying to hack me ?');
 define('IndexGoBack', 'Go back');
 ?>
