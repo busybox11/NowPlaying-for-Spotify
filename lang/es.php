3109f39e18bf92c9a18e73d7bf00ff5d7c5537e4
Voxan24 <Tutute@gmail.com>
2020-11-07 18:44:11 +0100

    Add spanish and Russian Translation

---
+++ lang/es.php
@@ -0,0 +1,20 @@
+<?php
+define('IndexTitle', 'Bienvenido a NowPlaying for Spotify !');
+define('IndexDescription', 'Por favor, inicia sesión con tu cuenta Spotify.');
+define('IndexConnection', 'Iniciar sesión en Spotify');
+define('IndexCookie', 'Haciendo clic en \'INICIAR SESION EN SPOTIFY\', usted acepta el uso de las cookies necesarias para el buen funcionamiento del sitio.');
+
+define('defaultTitleSong', 'No hay música');
+define('defaultArtistSong', 'Por favor, espere unos segundos para la actualización');
+define('defaultTitle', 'No hay música - Spotify Connect - Now Playing');
+define('ad', 'Publicidad');
+
+define('by', 'de');
+
+define('Error', 'Error');
+define('IndexError404', 'Esta página no existe. ¿No estás tratando de hackearme?');
+define('IndexGoBack', 'Regresa');
+
+define('streamTitleSong', 'No hay música');
+define('streamArtistSong', '');
+?>
