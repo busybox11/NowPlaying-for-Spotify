01f6c16fdc373818931d47c5e0186844b9e19fca
VoXaN24 <30030208+VoXaN24@users.noreply.github.com>
2020-11-08 20:06:22 +0100

    Update Russian Translation
    Add a missing Translation

--- lang/ru.php
+++ lang/ru.php
@@ -1,7 +1,7 @@
 <?php
 define('IndexTitle', 'Добро пожаловать в NowPlaying for Spotify !');
 define('IndexDescription', 'Пожалуйста, войдите в систему со своей учетной записью Spotify.');
-define('IndexConnection', 'Se connecter à Spotify');
+define('IndexConnection', 'Подключиться к Spotify');
 define('IndexCookie', 'Нажав на \'ПОДКЛЮЧИТЬСЯ К SPOTIFY\', вы принимаете использование cookie-файлов, необходимых для надлежащего функционирования сайта.');
 
 define('defaultTitleSong', 'Музыка не играет');
