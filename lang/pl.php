70647880af55e68f1ecd37947d5a7af99ddb6938
Voxan24 <Tutute@gmail.com>
2021-01-27 21:34:22 +0100

    Polonais

---
+++ lang/pl.php
@@ -0,0 +1,20 @@
+<?php
+define('IndexTitle', 'Witamy w NowPlaying for Spotify !');
+define('IndexDescription', 'Zaloguj się za pomocą swojego konta Spotify.');
+define('IndexConnection', 'Zaloguj się do Spotify');
+define('IndexCookie', 'Poprzez kliknięcie na \'Zaloguj się do Spotify\', wyrażasz zgodę na używanie plików cookies niezbędnych do prawidłowego funkcjonowania strony.');
+
+define('defaultTitleSong', 'Brak odtwarzania muzyki');
+define('defaultArtistSong', 'Proszę poczekać kilka sekund na aktualizację.');
+define('defaultTitle', 'Brak muzyki. - Spotify Connect - Now Playing');
+define('ad', 'Reklama');
+
+define('by', 'z');
+
+define('Error', 'Błąd');
+define('IndexError404', 'Ta strona nie istnieje. Czy nie próbujesz mnie zhakować?');
+define('IndexGoBack', 'Cofnij się');
+
+define('streamTitleSong', 'Brak muzyki.');
+define('streamArtistSong', '');
+?>
