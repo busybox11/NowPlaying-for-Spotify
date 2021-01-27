71cb8b3d4ec5dd26fd3b3327f6a3bef4b08480ba
Voxan24 <Tutute@gmail.com>
2021-01-27 21:37:54 +0100

    Japonais

---
+++ lang/jp.php
@@ -0,0 +1,20 @@
+<?php
+define('IndexTitle', 'NowPlaying for Spotifyようこそ !');
+define('IndexDescription', 'Spotifyのアカウントでログインしてください。');
+define('IndexConnection', 'Spotifyにログイン');
+define('IndexCookie', 'をクリックすることで \'SPOTIFYにログイン\', お客様は、本サイトの適切な機能のために必要なクッキーの使用に同意するものとします。.');
+
+define('defaultTitleSong', '音楽の演奏はありません');
+define('defaultArtistSong', '更新のために数秒お待ちください...。');
+define('defaultTitle', '音楽はありません。 - Spotify Connect - Now Playing');
+define('ad', '広告');
+
+define('by', 'から');
+
+define('Error', 'エラー');
+define('IndexError404', 'そのページは存在しません。私をハッキングしようとしてるんじゃないの？');
+define('IndexGoBack', '戻ろう');
+
+define('streamTitleSong', '音楽はありません。');
+define('streamArtistSong', '');
+?>
