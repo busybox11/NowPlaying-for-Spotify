71fb1bd276f182fcbfedebb9138c059d22c15260
French-Noodles <68303714+French-Noodles@users.noreply.github.com>
2021-01-31 14:30:10 +0200

    added arabic language

---
+++ lang/ar.php
@@ -0,0 +1,20 @@
+<?php
+define('IndexTitle', '!NowPlaying for Spotify مرحبًا بك في');
+define('IndexDescription', '.يرجى تسجيل الدخول باستخدام حساب Spotify الخاص بك');
+define('IndexConnection', 'Spotifyاتصل بـ');
+define('IndexCookie', 'بالنقر فوق \'CONNECT TO SPOTIFY \' ، فإنك تقبل استخدام ملفات تعريف الارتباط اللازمة للتشغيل السليم للموقع.');
+
+define('defaultTitleSong', 'ليس هناك موسيقى قيد التشغيل');
+define('defaultArtistSong', 'من فضلك انتظر بضع ثوان حتى يتم التشغيل');
+define('defaultTitle', 'لا موسيقى - Spotify Connect - Now Playing');
+define('ad', 'إعلانات');
+
+define('by', 'de'); //le "de" en arabe change beacoup avec les circonstanes donc il n'y a pas un mot/lettre que je peux mettre sans des circonstances specifiques
+
+define('Error', 'خطأ');
+define('IndexError404', 'هذه الصفحة غير موجودة. ألن تحاول اختراقي؟');
+define('IndexGoBack', 'عد الى الخلف');
+
+define('streamTitleSong', 'لا موسيقى');
+define('streamArtistSong', '');
+?>
