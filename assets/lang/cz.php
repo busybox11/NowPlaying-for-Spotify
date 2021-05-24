eb0bcfd7c810acd3bbb50fe41a7d2c2e7208499f
VoXaN24 <30030208+VoXaN24@users.noreply.github.com>
2021-05-24 21:03:17 +0200

    Create cz.php

---
+++ assets/lang/cz.php
@@ -0,0 +1,20 @@
+<?php
+define('IndexTitle', 'Vítejte v aplikaci NowPlaying pro Spotify!');
+define('IndexDescription', 'Přihlaste se pomocí svého účtu Spotify.');
+define('IndexConnection', 'Přihlášení se do Spotify');
+define('IndexCookie', 'Kliknutím na \'PŘIHLÁSIT SE DO SPOTIFY\' souhlasíte s používáním souborů cookie nezbytných pro správné fungování stránek.');
+
+define('defaultTitleSong', 'Žádná hudba se momentálně nepřehrává');
+define('defaultArtistSong', 'Počkejte prosím několik sekund, než se stav aktualizuje.');
+define('defaultTitle', 'No music - Spotify Connect - Now Playing');
+define('ad', 'Reklama');
+
+define('title_artistSong', "%title% vyrobený %artist%");
+
+define('Error', 'Error');
+define('IndexError404', 'Tato stránka neexistuje. Nesnažíte se mě hacknout?');
+define('IndexGoBack', 'Vrátit se zpět');
+
+define('streamTitleSong', 'Žádná hudba se momentálně nepřehrává');
+define('streamArtistSong', '');
+?>
