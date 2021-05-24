73e9b3a1aef7fef7ab2f3e8879cd493614ad4251
EarthlyEric6 <60834421+EarthlyEric@users.noreply.github.com>
2021-05-24 20:45:20 +0800

    Create zh_tw.php

---
+++ assets/lang/zh_tw.php
@@ -0,0 +1,21 @@
+  
+<?php
+define('IndexTitle', '歡迎來到 NowPlaying For Spotify!');
+define('IndexDescription', '請登入你的Spotify帳號');
+define('IndexConnection', '登入至Spotify');
+define('IndexCookie', 'By clicking on \'LOGIN TO SPOTIFY\', you accept the use of cookies necessary for the proper functioning of the site.');
+
+define('defaultTitleSong', 'No music currently playing');
+define('defaultArtistSong', 'Please wait a few seconds while status is updating');
+define('defaultTitle', 'No music - Spotify Connect - Now Playing');
+define('ad', 'Advertisement');
+
+define('title_artistSong', "%title% by %artist%");
+
+define('Error', 'Error');
+define('IndexError404', 'This page doesn\'t exist. Ain\'t you trying to hack me?');
+define('IndexGoBack', 'Go back');
+
+define('streamTitleSong', 'No music currently playing');
+define('streamArtistSong', '');
+?>
