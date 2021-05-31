25717c6ef52c95809250b8eba6a7fccba4158cdc
EarthlyEric6 <60834421+EarthlyEric@users.noreply.github.com>
2021-05-31 11:28:33 +0800

    Update zh_tw.php
    add all  繁體中文

--- assets/lang/zh_tw.php
+++ assets/lang/zh_tw.php
@@ -3,19 +3,19 @@
 define('IndexTitle', '歡迎來到 NowPlaying For Spotify!');
 define('IndexDescription', '請登入你的Spotify帳號');
 define('IndexConnection', '登入至Spotify');
-define('IndexCookie', 'By clicking on \'LOGIN TO SPOTIFY\', you accept the use of cookies necessary for the proper functioning of the site.');
+define('IndexCookie', '單擊“登錄到 SPOTIFY”，即表示您接受使用網站正常運行所需的 cookie');
 
-define('defaultTitleSong', 'No music currently playing');
-define('defaultArtistSong', 'Please wait a few seconds while status is updating');
+define('defaultTitleSong', '當前沒有音樂播放');
+define('defaultArtistSong', '狀態正在更新中，請等待幾秒鐘');
 define('defaultTitle', 'No music - Spotify Connect - Now Playing');
 define('ad', 'Advertisement');
 
-define('title_artistSong', "%title% by %artist%");
+define('title_artistSong', "%title% 演出者 %artist%");
 
-define('Error', 'Error');
-define('IndexError404', 'This page doesn\'t exist. Ain\'t you trying to hack me?');
-define('IndexGoBack', 'Go back');
+define('Error', '錯誤');
+define('IndexError404', '該頁面不存在。你不是想駭我嗎？');
+define('IndexGoBack', '返回');
 
-define('streamTitleSong', 'No music currently playing');
+define('streamTitleSong', '當前沒有音樂播放');
 define('streamArtistSong', '');
 ?>
