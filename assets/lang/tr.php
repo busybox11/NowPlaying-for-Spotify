aceddd75856bb0d3147da3c396b7511dec21ccf0
Yultax <48404411+Yultax@users.noreply.github.com>
2021-07-12 01:27:04 +0300

    Create tr.php

---
+++ assets/lang/tr.php
@@ -0,0 +1,20 @@
+<?php
+define('IndexTitle', 'Spotify için NowPlaying\'e hoşgeldiniz!');
+define('IndexDescription', 'Lütfen Spotify hesabınız ile giriş yapın.');
+define('IndexConnection', 'Spotify ile giriş yap');
+define('IndexCookie', '\'SPOTIFY İLE GİRİŞ YAP\' tıklayarak, sitenin düzgün çalışması için gerekli olan çerezlerin kullanımını kabul etmiş olursunuz.');
+
+define('defaultTitleSong', 'Herhangi bir müzik çalmıyor.');
+define('defaultArtistSong', 'Durumunuz güncellenirken lütfen birkaç saniye bekleyin');
+define('defaultTitle', 'Müzik yok - Spotify Connect - Now Playing');
+define('ad', 'Reklam');
+
+define('title_artistSong', "%artist% tarafından %title%");
+
+define('Error', 'Hata');
+define('IndexError404', 'Böyle bir sayfa bulunmamakta. Beni hacklemeye mi çalışıyorsun?');
+define('IndexGoBack', 'Geri git');
+
+define('streamTitleSong', 'Herhangi bir müzik çalmıyor');
+define('streamArtistSong', '');
+?>
