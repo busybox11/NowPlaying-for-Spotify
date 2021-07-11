c574a1a8df5db994a194c2d4fee457782ba63220
Yultax <48404411+Yultax@users.noreply.github.com>
2021-07-11 16:26:04 +0300

    Update index.php

--- index.php
+++ index.php
@@ -58,6 +58,7 @@ if(isset($_GET['lang'])){
 				<a href="?lang=id"><img src="assets/images/indonesia.png" title="Indonesia" class="flag_icons"></a>
 				<a href="?lang=cz"><img src="assets/images/czech.png" title="Czech" class="flag_icons"></a>
 				<a href="?lang=gr"><img src="assets/images/greece.png" title="Greece" class="flag_icons"></a>
+				<a href="?lang=tr"><img src="assets/images/turkey.png" title="Turkish" class="flag_icons"></a>
 				<a href="?lang=zh_tw"><img src="assets/images/taiwan.png" title="Traditionnal Chinese" class="flag_icons"></a>
 			</div>
 			<p class="space20"></p>
