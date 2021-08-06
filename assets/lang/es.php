77ec21e2068fa4c95d338ab2aedc152d7f1efcc0
Victor B <victor.bonnelle@protonmail.com>
2021-08-06 14:31:20 +0200

    Add missing accent in spanish translation

--- assets/lang/es.php
+++ assets/lang/es.php
@@ -2,7 +2,7 @@
 define('IndexTitle', 'Bienvenido a NowPlaying for Spotify !');
 define('IndexDescription', 'Por favor, inicia sesión con tu cuenta Spotify.');
 define('IndexConnection', 'Iniciar sesión en Spotify');
-define('IndexCookie', 'Haciendo clic en \'INICIAR SESION EN SPOTIFY\', usted acepta el uso de las cookies necesarias para el buen funcionamiento del sitio.');
+define('IndexCookie', 'Haciendo clic en \'INICIAR SESIÓN EN SPOTIFY\', usted acepta el uso de las cookies necesarias para el buen funcionamiento del sitio.');
 
 define('defaultTitleSong', 'No hay música');
 define('defaultArtistSong', 'Por favor, espere unos segundos para la actualización');
