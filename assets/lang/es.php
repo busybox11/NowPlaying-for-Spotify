86a9a6c01b755e052c2503d8ae1b1e84c9ad0c44
Diego Barreiro Perez <diego@barreiro.dev>
2025-04-15 01:09:46 +0100

    Update Spanish Translations
    Correct spelling mistakes and improve tone consistency.

--- assets/lang/es.php
+++ assets/lang/es.php
@@ -1,11 +1,11 @@
 <?php
-define('IndexTitle', 'Bienvenido a NowPlaying for Spotify !');
+define('IndexTitle', '¡Bienvenido a NowPlaying for Spotify!');
 define('IndexDescription', 'Por favor, inicia sesión con tu cuenta Spotify.');
 define('IndexConnection', 'Iniciar sesión en Spotify');
-define('IndexCookie', 'Haciendo clic en \'INICIAR SESIÓN EN SPOTIFY\', usted acepta el uso de las cookies necesarias para el buen funcionamiento del sitio.');
+define('IndexCookie', 'Haciendo clic en \'INICIAR SESIÓN EN SPOTIFY\', aceptas el uso de las cookies necesarias para el buen funcionamiento del sitio.');
 
 define('defaultTitleSong', 'No hay música');
-define('defaultArtistSong', 'Por favor, espere unos segundos para la actualización');
+define('defaultArtistSong', 'Por favor, espera unos segundos para la actualización');
 define('defaultTitle', 'No hay música - Spotify Connect - Now Playing');
 define('ad', 'Publicidad');
 
@@ -13,7 +13,7 @@ define('title_artistSong', "%title% de %artist%");
 
 define('Error', 'Error');
 define('IndexError404', 'Esta página no existe. ¿No estás tratando de hackearme?');
-define('IndexGoBack', 'Regresa');
+define('IndexGoBack', 'Volver');
 
 define('streamTitleSong', 'No hay música');
 define('streamArtistSong', '');
